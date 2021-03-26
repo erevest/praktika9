37946c557bdfde86764a5af94069677871cf2ce8
Андрей Раскалов <erevestroofs@hotmail.com>
2021-03-26 17:01:57 +0300

    dom

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -28,13 +28,25 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.Button3 = new System.Windows.Forms.Button();
       this.SuspendLayout();
+      // 
+      // Button3
+      // 
+      this.Button3.Location = new System.Drawing.Point(797, 12);
+      this.Button3.Name = "Button3";
+      this.Button3.Size = new System.Drawing.Size(75, 23);
+      this.Button3.TabIndex = 0;
+      this.Button3.Text = "Button3";
+      this.Button3.UseVisualStyleBackColor = true;
+      this.Button3.Click += new System.EventHandler(this.Button3_Click);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(884, 560);
+      this.Controls.Add(this.Button3);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,7 +55,9 @@
 
     }
 
-    #endregion
-  }
+        #endregion
+
+        private System.Windows.Forms.Button Button3;
+    }
 }
 
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp3
   {
     Point[] points = new Point[50];
     Pen pen = new Pen(Color.Black, 2);
+    int i;
     public Form1()
     {
       InitializeComponent();
@@ -22,24 +23,45 @@ namespace WindowsFormsApp3
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
       Graphics g = e.Graphics;
-      g.DrawLines(pen, points);
+
+      
+
+      
+
+      SolidBrush brush1 = new SolidBrush(Color.LightBlue);
+      Rectangle[] rects1 = { new Rectangle(0, 0, 1000, 1000) };
+      e.Graphics.FillRectangles(brush1, rects1);
+
+      //синие квадраты
+
+      SolidBrush blueBrush = new SolidBrush(Color.Gray);
+      Rectangle[] rects = { new Rectangle(25, 125, 500, 500), new Rectangle(475, 450, 50, 100) };
+      // Fill rectangles to screen.
+      e.Graphics.FillRectangles(blueBrush, rects);
+
+    
+
+      g.DrawLine(new Pen(Brushes.Black, 2), 10, 4* i +20, 500, 4*i+20);
+      
+      g.DrawRectangle(new Pen(Brushes.Blue, 2), 475, 450, 50, 100);
+      g.DrawEllipse(new Pen(Brushes.Black, 4), 200, 200, 60, 60);
+      
+      //пунктирная линия
+      float[] dashValues = { 5, 2, 15, 4 };
+      Pen blackPen = new Pen(Color.Black, 5);
+      blackPen.DashPattern = dashValues;
+      e.Graphics.DrawLine(blackPen, new Point(5, 5), new Point(405, 5));
+      //ещё одна линия
+      
     }
 
     private void Form1_Load(object sender, EventArgs e)
     {
-      for (int i = 0; i < 20; i++)
-      {
-        int xPos;
-        if (i % 2 == 0)
-        {
-          xPos = 10;
-        }
-        else
-        {
-          xPos = 400;
-        }
-        points[i] = new Point(xPos, 10 * i);
-      }
+    }
+
+    private void Button3_Click(object sender, EventArgs e)
+    {
+      
     }
   }
 }
